e93df6bb9d8eb1df82093e124c3d899850a6e589
Erik-PC\Erik <eeverson@msudenver.edu>
2017-03-08 14:58:52 -0700

    Initial setup of classes complete

---
+++ test/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,16 @@
+package iloveyouboss;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Erik
+ */
+public class ScoreCollectionTest {
+    @Test
+    public void arithmeticMean() throws Exception {
+
+    }
+
+}
